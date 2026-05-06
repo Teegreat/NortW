8e5879eaed2dc256366e2d53eaadcee09eb77410
Teegreat <toyinatofarati64@gmail.com>
2026-05-06 04:44:13 +0100

    fixed deploy issues

--- backend/src/webhooks/polar.ts
+++ backend/src/webhooks/polar.ts
@@ -1,4 +1,4 @@
-import type { Request, Response, NextFunction } from "express";
+import type { Request, Response } from "express";
 import { getEnv } from "../lib/env";
 import { checkoutSessions, orderItems, orders } from "../db/schema";
 import { eq } from "drizzle-orm";
