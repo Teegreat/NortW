ac8dea0954497e19ac07efed00abb4bdcb6a15d1
Teegreat <toyinatofarati64@gmail.com>
2026-05-10 11:03:20 +0100

    orders page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,13 @@ import {
 } from "@clerk/react";
 import PageLoader from "./components/PageLoader";
 import Layout from "./components/Layout";
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
+import OrdersPage from "./pages/OrdersPage";
 
 function App() {
-  const { isLoaded } = useAuth();
+  const { isLoaded, isSignedIn } = useAuth();
 
   if (!isLoaded) return <PageLoader />;
 
@@ -21,6 +22,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/orders" element={isSignedIn ? <OrdersPage /> : <Navigate to={"/"} />} replace />
       </Routes>
     </Layout>
   );
