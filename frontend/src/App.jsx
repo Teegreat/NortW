4e1715e879dc0236902fe51e5dbdb571b5a38436
Teegreat <toyinatofarati64@gmail.com>
2026-05-10 11:39:28 +0100

    product detail page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
 import OrdersPage from "./pages/OrdersPage";
 import CheckoutReturnPage from "./pages/CheckoutReturnPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
 
 function App() {
   const { isLoaded, isSignedIn } = useAuth();
@@ -23,16 +24,13 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/product/:slug" element={<ProductDetailPage />} />
         <Route
           path="/orders"
           element={isSignedIn ? <OrdersPage /> : <Navigate to={"/"} />}
           replace
         />
-        <Route
-          path="/checkout/return"
-          element={<CheckoutReturnPage  />}
-        
-        />
+        <Route path="/checkout/return" element={<CheckoutReturnPage />} />
       </Routes>
     </Layout>
   );
